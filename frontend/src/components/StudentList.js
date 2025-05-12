a49fabfe1a332d75141e5f9ff8ca4d173e471d2f
roshi12 <abdullah.roshaan@gmail.com>
2025-05-12 19:36:17 +0500

    Add Dockerfiles and Kubernetes deployment/service files

--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getStudents, deleteStudent } from '../services/studentService';
+import { getStudents, deleteStudent } from '../../services/studentService';
 
 const StudentList = () => {
   const [students, setStudents] = useState([]);
